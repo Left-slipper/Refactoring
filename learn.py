b3d8b2f85cc8ced21f54cf21ce1dae48ebc76e0d
SC-Polus <115619810+SC-Polus@users.noreply.github.com>
2022-10-14 16:51:29 +0700

    Refactoring 1
    удалены не используемые функции, что бы привести код в порядок, также удалены неиспользуемые переменные. Убраны не нужные комментарии повторяющиеся строки

--- learn.py
+++ learn.py
@@ -17,49 +17,7 @@ path_to_gistagram = "OR_gistagram/"
 def clearFilesForFolder(path):
     for file in glob(path):
         os.remove(file)
-        
-
-def calcGradients(img, format):
-    global path_to_gradient
-    
-    I = cv.resize(img, dsize=[10,10])
-    #gradient = np.gradient(copy(img))
-    
-    gradients = []
-    list = []
-    
-    for i in range(10):
-        for j in range(9):
-            
-            list.append(I[i][j])
-            list.append(I[i][j+1])
-            
-            gradients.append([int(list[0][0]) - int(list[1][0]), int(list[0][1]) - int(list[1][1]), int(list[0][2]) - int(list[1][2])])
-            #print(gradients[j])
-            list.clear()
-            
-       
-        
-    with open(path_to_gradient +str(format[0]) + ".json", 'w', encoding='utf-8') as fw:
-        json.dump(gradients, fw)
-        
-
-def calcGrayHist(I, format):
-         # Рассчитать серую гистограмму
-    global path_to_gistagram
-    
-    I = cv.resize(I, dsize=[20,20])
-    
-    h, w = I.shape[:2]
-    grayHist = np.zeros([256], np.uint64)
-    
-    for i in range(h):
-        for j in range(w):
-            grayHist[I[i][j]] += 1
-    gistagram = grayHist
-    
-    with open(path_to_gistagram +str(format[0]) + ".json", 'w', encoding='utf-8') as fw:
-        json.dump(gistagram.tolist(), fw)
+                
         
 
 def calcDescripters(image, format):
@@ -84,50 +42,16 @@ def calcDescripters(image, format):
 
 def getDataForImage(img, filename):
     global path_to_vector, path_to_gradient, path_to_gistagram
-    
-    #gausimg = cv.GaussianBlur(img, (5,5), 0)
-    image = img
-    #image = cv.resize(img, dsize=[500,500])
-    #image = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
-    #print(ntpath.dirname(img))
-    
-    #cv.imshow("image", image)
-    
-    #print(gradient.split('.'))
+
     # Инициация орб
     
     format = filename.split(".")
-    
-    threading.Thread(target= lambda: calcGrayHist(img, format), name='D').start()
-    threading.Thread(target= lambda: calcGradients(img, format), name='F').start()
+
     threading.Thread(target= lambda:  calcDescripters(img, format), name='R').start()
-    #calcGrayHist(img, format)
-    #calcGradients(img, format)
-    
-    
-
-"""    string = ""
-    for i in des:
-        string += str(i) + " "
-    
-
-    file = open(path_to_vector + str(format[0]) + ".txt", 'w')
-    file.write(string)
-    file.close()"""
-    
-    # нарисовать
-    #img2 = cv.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
-    #cv.imshow("image", img2)
-    #cv.waitKey(0)
 
 
-#threading.Thread(target= lambda: clearFilesForFolder("OR_vector/*"), name='A').start()
-#threading.Thread(target= lambda: clearFilesForFolder("OR_gradient/*"), name='B').start()
-#threading.Thread(target= lambda: clearFilesForFolder("OR_gistagram/*"), name='C').start()
 path = "image3/"
-for filename in os.listdir(path):
-    #img = cv.imread(os.path.join(path, filename))
-    #img = cv.imdecode(np.fromfile(os.path.join(path, filename), dtype=np.uint8), cv.IMREAD_GRAYSCALE)      
+for filename in os.listdir(path):   
     img = cv.imdecode(np.fromfile(os.path.join(path, filename), dtype=np.uint8), cv.IMREAD_COLOR)      
        
     if img is not None:
--- work.py
+++ work.py
@@ -11,75 +11,6 @@ import time
 
 path = "binary_photo/"
 thresh = 145
-
-def detected_gradient(img, gradients, names):
-    return False
-    
-    img = cv.resize(img, dsize=[10,10])
-
-    for gradCount in range(len(gradients)):        
-        resultGrad = 0 
-        count = 0
-        
-        for i in range(10):
-            for j in range(9):
-
-                gradImg = [int(img[i][j][0]) - int(img[i][j+1][0]), int(img[i][j][1]) - int(img[i][j+1][1]), int(img[i][j][2]) - int(img[i][j+1][2])] 
-                
-                #print(sum(gradImg))
-                #print(sum(gradients[gradCount][count]))
-                print("Grad = " , abs(sum(gradients[gradCount][count]) - sum(gradImg)))
-                print("Atalon = ", abs(sum(gradients[gradCount][count]))*0.4)
-                
-                if(abs(sum(gradients[gradCount][count]) - sum(gradImg)) <= abs(sum(gradients[gradCount][count]))*0.4):
-                    resultGrad += 1
-                count += 1
-                
-        print("resultGrad = ", resultGrad)
-    
-        if(resultGrad >= 15):
-            print( names[gradCount])
-            time.sleep(3)  # import time
-            return True
-        else: return False
-        
-      
-
-def detected_gistagram(img, gistagrams, names):
-    return False
-    
-    img = cv.resize(img, dsize=[20,20])
-    h, w = img.shape[:2]
-    grayHist = np.zeros([256], np.uint64)
-    for i in range(h):
-        for j in range(w):
-            grayHist[img[i][j]] += 1
-    
-   
-    name_result = ""     
-    for gist, name in zip(gistagrams, names):
-        resultGist = 0   
-        
-        if (len(grayHist) < len(gist)):
-            grayHist = np.interp(gist, grayHist, grayHist)
-        elif (len(gist) < len(grayHist)):
-            gist = np.interp(grayHist, gist, gist)         
-        
-        for i in range(len(gist)):
-            
-            if (abs(gist[i] - grayHist[i]) <= gist[i]*0.07):
-                resultGist += 1
-            # какой критерий сравнение? 
-            # необходима интерполяция гистограмм           
-        
-        print(resultGist, " " + name)  
-        if(resultGist >= 90): 
-            print(name)   
-            return True
-       
-                             
-        
-    return False
                         
             
     
@@ -106,15 +37,10 @@ def vector_comparison(dataVectors, img, names, method='ORB'):
         goodDescript.clear()
         vector = np.uint8(vector)
         matches = bf.knnMatch(des,vector, k=2)
-        #matches = bf.match(vector, des)
-        #matches = sorted(matches, key = lambda x:x.distance)
 
         for m,n in matches:
             if m.distance < lowe_ratio*n.distance:
                 goodDescript.append([m])
-        #for i,m inна enumerate(matches):
-            #if i < len(matches) - 1 and m.distance < lowe_ratio * matches[i+1].distance:
-                #goodDescript.append([m])
         
         if(len(goodDescript) >= 65):
             print(datetime.now() - start_time)
@@ -127,30 +53,26 @@ def vector_comparison(dataVectors, img, names, method='ORB'):
     return False
         
 
-# Функция основная обрабатывующая 
+
 def searchLineObject(img, vectors, names, gistagram, gradients):
     global path, thresh 
-    #cv.imshow("image", img)
     key = 0
 
     # Обработка изображения, изменение контрастности
     enhancer = ImageEnhance.Contrast(Image.fromarray(img, 'RGB'))
     enhancer_img = np.asarray(enhancer.enhance(1.5))
-    #cv.imshow("img", enhancer_img)   
+ 
         
     imageGray = cv.cvtColor(enhancer_img, cv.COLOR_BGR2GRAY)  
     imgGray = imageGray.reshape(len(imageGray), len(imageGray[0]))
     imgGray = np.array([imgGray,imgGray,imgGray])
     imgGray = np.moveaxis(imgGray, 0, -1)
     
-    #img_binary_1 = gs(np.asarray(imgGray), 2)
+
     gausimg = cv.GaussianBlur(imgGray, (5,5), 0)
     img_binary = cv.threshold(gausimg, thresh, 255, cv.THRESH_BINARY)[1]
-            
-    #cv.imshow("image", img_binary)
 
-    #cv.imwrite(path+name, cv.threshold(img_binary, 165, 255, cv.THRESH_BINARY)[1])
-    #cv.imwrite(path+name, img_binary)
+
 
     lower = [255, 255, 255]
     upper = [255, 255, 255]
@@ -169,10 +91,9 @@ def searchLineObject(img, vectors, names, gistagram, gradients):
     
     if len(contours) != 0:
         c = max(contours, key = len)
-        #output = img
+
     
         x,y,w,h = cv.boundingRect(c)
-        #res_binary = img_binary[y:(y+h-30),x:(x+h-45)]
         res_img = img[y+25:(y+h-100),x+25:(x+h-120)]
         cv.imshow("result", res_img)
         res_grey_img = imgGray[y+25:(y+h-100),x+25:(x+h-120)]
@@ -184,35 +105,21 @@ def searchLineObject(img, vectors, names, gistagram, gradients):
         cv.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
 
     cv.imshow("cascade_image", output)
-    #cv.imshow("res_gaus_img", res_gaus_img)
+
     (width, height) = res_img.shape[:2]
     cv.imshow("result_image", img_binary)
-    #cv.imshow("result_image2", res_img)
-    
-       
-    #if(width >= 200 and height >= 270):
+
+
     if(vector_comparison(vectors, res_gaus_img, names, 'ORB')):
         key = 0
         cv.destroyAllWindows()
-        #cv.imshow("res_gaus_img", res_gaus_img)
+
         cv.imshow("result", res_img)
         cv.waitKey(0)  
         
-        #if(detected_gistagram(res_img, gistagram, names)):
-            #cv.destroyAllWindows()
-            #cv.imshow("result", res_img)
-            #cv.waitKey(0) 
-            
-        #if(detected_gradient(res_img, gradients, names)):
-         #   cv.destroyAllWindows()
-          #  cv.imshow("result", res_img)
-           # cv.waitKey(0) 
-
 
     return key, res_img
-    
 
-#searchLineObject(cv.imread("Photo/236.jpg"), "0.jpg")
 cv.waitKey()
 cv.destroyAllWindows()
 
